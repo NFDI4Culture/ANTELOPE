0cb725091a7618594e37285f328f54585640eaa9
t-ski <t.schiepanski@gmx.de>
2024-05-21 14:35:36 +0200

    Disable auto show sidebar after manual toggle

--- frontend/src/components/InfoComponent.vue
+++ frontend/src/components/InfoComponent.vue
@@ -1,93 +1,89 @@
 <script setup lang="ts">
-	import { Ref, ref, onMounted } from "vue";
+import { Ref, ref, onMounted } from 'vue';
 
-	const showHover = ref(false);
-	const infoEl: Ref<HTMLElement> = ref(null);
-	const hoverEl: Ref<HTMLElement> = ref(null);
-    const empty = ref(true);
+const showHover = ref(false);
+const infoEl: Ref<HTMLElement> = ref(null);
+const hoverEl: Ref<HTMLElement> = ref(null);
+const empty = ref(true);
 
-    onMounted(() => {
-        hoverEl.value
-        .parentNode.removeChild(hoverEl.value);
-        document.body.appendChild(hoverEl.value);
-        
-        empty.value = !hoverEl.value.textContent.trim().length;
-    });
+onMounted(() => {
+  hoverEl.value.parentNode.removeChild(hoverEl.value);
+  document.body.appendChild(hoverEl.value);
 
-    function adaptHover(keepALive = false) {
-        if(!keepALive) return;
+  empty.value = !hoverEl.value.textContent.trim().length;
+});
 
-        const position = infoEl.value
-        .getBoundingClientRect();
-        const offset = 30;
-        hoverEl.value
-        .style.top = `${Math.min(position.y, window.innerHeight - hoverEl.value.offsetHeight - offset)}px`;
-        hoverEl.value
-        .style.left = `${Math.max(position.x - hoverEl.value.offsetWidth, offset)}px`;
-        
-        window.requestAnimationFrame(() => adaptHover(showHover.value));
-    }
+function adaptHover(keepALive = false) {
+  if (!keepALive) return;
 
-    function toggleHover() {
-        adaptHover(!showHover.value);
+  const position = infoEl.value.getBoundingClientRect();
+  const offset = 30;
+  hoverEl.value.style.top = `${Math.min(position.y, window.innerHeight - hoverEl.value.offsetHeight - offset)}px`;
+  hoverEl.value.style.left = `${Math.max(position.x - hoverEl.value.offsetWidth, offset)}px`;
 
-        showHover.value = !showHover.value;
-    }
+  window.requestAnimationFrame(() => adaptHover(showHover.value));
+}
+
+function toggleHover() {
+  adaptHover(!showHover.value);
+
+  showHover.value = !showHover.value;
+}
 </script>
 
 <template>
-    <div v-show="!empty" class="info" @mouseenter="toggleHover()" @mouseout="toggleHover()" ref="infoEl">
-        <i class="info-icon"></i>
-        <p class="info-hover" :class="showHover ? 'active' : ''" ref="hoverEl">
-            <slot></slot>
-        </p>
-    </div>
+  <div v-show="!empty" class="info desktop" @mouseenter="toggleHover()" @mouseout="toggleHover()" ref="infoEl">
+    <i class="info-icon"></i>
+    <p class="info-hover" :class="showHover ? 'active' : ''" ref="hoverEl">
+      <slot></slot>
+    </p>
+  </div>
 </template>
 
 <style scoped lang="scss">
-	@import "@/assets/scss/shared";
-    
-    .info {
-        overflow-wrap: break-word;
-                
-        &-icon {
-            position: relative;
-            z-index: 1;
-            
-            @include icon("info");
-        }
-        &-hover {
-            position: fixed;
-            padding: 0.4rem 0.6rem;
-            margin-top: calc(0.75rem + var(--space-xs));
-            margin-left: calc(-0.75 * var(--space-xs));
-            max-width: 300px;
-            line-height: 1.25em;
-            background-color: var(--color-bg-dark);
-            color: var(--color-bg-light);
-            font-weight: 400;
-            text-align: justify;
-            text-transform: none;
-            border-radius: var(--border-radius-s);
-            transform: translateY(10%);
-            transition: filter 200ms, transform 200ms;
-            transition-delay: 100ms;
-            pointer-events: none;
-            user-select: none;
-            z-index: 100;
-            filter: opacity(0);
+@import '@/assets/scss/shared';
 
-            @include font-s;
+.info {
+  overflow-wrap: break-word;
 
-            &.active {
-                transform: translateY(0);
-                filter: opacity(0.875);
-            }
-        }
+  &-icon {
+    position: relative;
+    z-index: 1;
+
+    @include icon('info');
+  }
+  &-hover {
+    position: fixed;
+    padding: 0.4rem 0.6rem;
+    margin-top: calc(0.75rem + var(--space-xs));
+    margin-left: calc(-0.75 * var(--space-xs));
+    max-width: 300px;
+    line-height: 1.25em;
+    background-color: var(--color-bg-dark);
+    color: var(--color-bg-light);
+    font-weight: 400;
+    text-align: justify;
+    text-transform: none;
+    border-radius: var(--border-radius-s);
+    transform: translateY(10%);
+    transition: filter 200ms, transform 200ms;
+    transition-delay: 100ms;
+    pointer-events: none;
+    user-select: none;
+    z-index: 100;
+    filter: opacity(0);
+
+    @include font-s;
+
+    &.active {
+      transform: translateY(0);
+      filter: opacity(0.875);
     }
+  }
+}
 </style>
 <style lang="scss">
-    *:not(:empty) .info {
-        margin-left: 0.5em;
-    }
+*:not(:empty) .info {
+  margin-left: 0.5em;
+}
 </style>
--- frontend/src/views/HomeView/HomeView.vue
+++ frontend/src/views/HomeView/HomeView.vue
@@ -30,7 +30,6 @@ EntitySelectionService.on('select', () => {
   showSidebarNotification.value = !showSidebar.value;
 
   if (!toggleSidebarOnSelect || showSidebar.value) return;
-  toggleSidebarOnSelect = false;
 
   toggleSidebar();
 });
@@ -50,6 +49,8 @@ function checkScroll() {
 }
 
 function toggleSidebar() {
+  toggleSidebarOnSelect = false;
+
   showSidebar.value = !showSidebar.value;
 
   showSidebarNotification.value = false;
@@ -77,8 +78,8 @@ function uncollapse() {
         </h1>
         <nav>
           <ol>
+            <li><a @click="integrationOverlayEl.toggle()" class="desktop">Integration</a></li>
             <li><a @click="aboutOverlayEl.toggle()">About</a></li>
-            <li><a @click="integrationOverlayEl.toggle()">Integration</a></li>
             <li><a href="/api.html" target="_blank">API</a></li>
             <li class="scroll-keep desktop">
               <a
