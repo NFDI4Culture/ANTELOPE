57da17e9b73c01e424a9c7d9bd295058159169a3
t-ski <t.schiepanski@gmx.de>
2024-05-27 13:29:56 +0200

    Add search tab change results history

--- frontend/src/services/RestService.ts
+++ frontend/src/services/RestService.ts
@@ -1,14 +1,13 @@
 import { EnvService } from "./EnvService";
 
-type TAtomic = string|number|boolean;
-type TSearchParams = { [ key: string ]: TAtomic; };
-type THeaders = { [ key: string ]: string; };
-
+type TAtomic = string | number | boolean;
+type TSearchParams = { [key: string]: TAtomic };
+type THeaders = { [key: string]: string };
 
 export class Request<T> {
-    private static MOCK_FALLBACK_ENABLED: boolean = true;	// Set to false to test failure behaviour without mock intercept
+	private static MOCK_FALLBACK_ENABLED: boolean = true; // Set to false to test failure behaviour without mock intercept
 
-    private static baseUrl = `${document.location.origin.replace(/(:\d+)?$/, ":8080")}/api`;
+	private static baseUrl = `${document.location.origin.replace(/(:\d+)?$/, ":8080")}/api`;
 
 	private readonly method: string;
 	private readonly endpoint: string;
@@ -25,50 +24,41 @@ export class Request<T> {
 		this.body = body;
 		this.headers = headers;
 	}
-	
-    public call(): Promise<T> {
-		console.log(this.body)
+
+	public call(): Promise<T> {
 		return new Promise((resolve, reject) => {
 			const url = `${Request.baseUrl}${this.endpoint.replace(/^\/?/, "/")}${
 				this.searchParams
-				? `?${
-					Object.entries(this.searchParams)
-					.map((param: [ string, TAtomic ]) => `${encodeURI(param[0])}=${encodeURI(param[1].toString())}`)
-					.join("&")
-				}`
-				: ""
+					? `?${Object.entries(this.searchParams)
+					.map((param: [string, TAtomic]) => `${encodeURI(param[0])}=${encodeURI(param[1].toString())}`)
+					.join("&")}`
+					: ""
 			}`;
 			fetch(url, {
 				method: this.method.toUpperCase(),
 				mode: "cors",
 				cache: "no-cache",
 				headers: {
-					...!(this.body instanceof FormData) ? { "Content-Type": "application/json" } : {},
-										
-					...this.headers ?? {}
+					...(!(this.body instanceof FormData) ? { "Content-Type": "application/json" } : {}),
+
+					...(this.headers ?? {}),
 				},
-				
-				... this.body
-				? {
-					body: !(this.body instanceof FormData) ? JSON.stringify(this.body): this.body
-				}
-				: {}
+
+				...(this.body
+					? {
+						body: !(this.body instanceof FormData) ? JSON.stringify(this.body) : this.body,
+					}
+					: {}),
 			})
 			.then(res => res.json())
 			.then(obj => {
 				resolve(obj as T);
 			})
 			.catch(err => {
-				if(EnvService.IS_DEV
-				&& Request.MOCK_FALLBACK_ENABLED
-				&& this.mockResponse) {
-					console.log(`${
-						this.method.toUpperCase()
-					}: ${
-						url
-					}${this.body ? ":" : ""}`);
+				if (EnvService.IS_DEV && Request.MOCK_FALLBACK_ENABLED && this.mockResponse) {
+					console.log(`${this.method.toUpperCase()}: ${url}${this.body ? ":" : ""}`);
 					this.body && console.log(this.body);
-					
+
 					console.warn("Request has assigned a development mock response that is used in place of applying the default behavior.");
 
 					setTimeout(() => resolve(this.mockResponse as T), 1000);
@@ -79,7 +69,7 @@ export class Request<T> {
 				reject(err);
 			});
 		});
-    }
+	}
 
 	public mock(fallbackResponse: T): this {
 		this.mockResponse = fallbackResponse;
@@ -88,8 +78,8 @@ export class Request<T> {
 	}
 }
 
-
 export const RestService = {
 	GET: (endpoint: string, searchParams?: TSearchParams, headers?: THeaders) => new Request("GET", endpoint, searchParams, headers),
-	POST: (endpoint: string, searchParams?: TSearchParams, body?: unknown, headers?: THeaders) => new Request("POST", endpoint, searchParams, body, headers)
+	POST: (endpoint: string, searchParams?: TSearchParams, body?: unknown, headers?: THeaders) =>
+		new Request("POST", endpoint, searchParams, body, headers),
 };
